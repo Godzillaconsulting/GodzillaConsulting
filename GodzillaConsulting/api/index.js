8df733c4040f1785c3a1a9810fd4748caaa36871
Godzillaconsulting <godzilla.oscar21@gmail.com>
2026-03-13 16:42:13 -0600

    feat: wrapper try catch para api en Vercel Serverless

--- GodzillaConsulting/api/index.js
+++ GodzillaConsulting/api/index.js
@@ -1,4 +1,13 @@
-
-import app from '../server/index.js';
-
-export default app;
+export default async function handler(req, res) {
+    try {
+        const module = await import('../server/index.js');
+        const app = module.default;
+        return app(req, res);
+    } catch (error) {
+        return res.status(500).json({
+            status: "CRASH_Vercel_Boot",
+            error: error.message,
+            stack: error.stack
+        });
+    }
+}
